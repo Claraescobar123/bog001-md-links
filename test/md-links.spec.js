1c592d81b1fb602958b63129d181987c5157aa1a
Clara <ing.clara.93@gmail.com>
2020-11-10 12:06:50 -0500

    cambios prefinales

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,22 @@
-const mdLinks = require('../');
+const mdLinks = require('../lib/index.js');
+const searchLinks = require('../lib/index.js');
+const axios = require ('axios');
+const path = require('path');
+const mockFilePath = path.resolve('./prueba.md');
 
+jest.mock('axios');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should be a function', () => {
+    expect(typeof mdLinks).toBe('function');
+    console.log('OK, this is a function');
   });
 
+
+it('Array with md files', () => {
+  return mdLinks(mockFilePath, {validate:false, stats:false}).then((res) =>{
+    expect(res).toStrictEqual(searchLinks);
+  });
+});
 });
